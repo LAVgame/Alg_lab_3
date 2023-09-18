d27f140b2907efdf778f5c2a2341e5e91cbc9bad
LAVgame <vipaleksei007@gmail.com>
2023-09-18 15:27:37 +0300

    new

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = Stopwatch.StartNew();
+            sw.Start();
+            Console.WriteLine("Введи размерность массива");
+            int n = int.Parse(Console.ReadLine());
+            int[] Arr = new int[n];
+            Random rnd = new Random();
+            Console.Write("Какое число желаете найти?: ");
+            int k = int.Parse(Console.ReadLine());
+            for (int i = 0; i< n; i++) 
+            {
+                Arr[i] = rnd.Next(-20,20);
+                if (Arr[i] == k) Console.WriteLine($"Arr[{i}] = {Arr[i]}");
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Время работы: {sw.Elapsed}" );
+        }
+    }
+}
